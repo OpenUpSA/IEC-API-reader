9f8aa0695dbffaa877b1db1e3aabee7699add739
Jason Norwood-Young <jason@10layer.com>
2014-04-23 13:28:41 +0200

    Sample config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.username = "IECUsername";
+config.password = "IECPassword";
+
+config.apiUrl = "https://api.elections.org.za";
+config.userAgent = "Code4SA";
+
+config.port = 8082;
+
+module.exports = config;
